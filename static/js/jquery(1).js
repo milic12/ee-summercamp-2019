1815ed563962e9e8665fba47cd1c33261d1893b0
Manojlo Ilić <50624481+milic12@users.noreply.github.com>
2019-06-01 20:18:27 +0200

    Add files via upload

---
+++ static/js/jquery(1).js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function(){
+	
+	function checkAnimation() {
+    var $elem = $('.bar .level');
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    } else {
+        $elem.removeClass('start');
+    }
+}
+	
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},11000);
+	});
+});
